d626c4f38a59604f41c919875d53b27da0055df1
Michael Waldo <mbwcogic@yahoo.com>
2020-01-19 14:14:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,6 +31,6 @@ class Song
     self.find_by_name(song_name) || self.create_by_name(song_name)
   end
   def self.alphabetical 
-    self.all.sort_by { |so| word.length }
+    self.all.sort_by { |song| song.length }
 end 
 
